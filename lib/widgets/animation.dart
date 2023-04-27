54dd4caa908cdd953630b1676a429ed90dd3364b
KadirovKmck <kadirovmuhammadaziz79@gmail.com>
2023-04-27 16:04:31 +0600

    shdg

---
+++ lib/widgets/animation.dart
@@ -0,0 +1,13 @@
+return SizedBox(
+  width: 250.0,
+  child: TextLiquidFill(
+    text: 'LIQUIDY',
+    waveColor: Colors.blueAccent,
+    boxBackgroundColor: Colors.redAccent,
+    textStyle: TextStyle(
+      fontSize: 80.0,
+      fontWeight: FontWeight.bold,
+    ),
+    boxHeight: 300.0,
+  ),
+);
